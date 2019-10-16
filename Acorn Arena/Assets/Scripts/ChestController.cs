4c2f9982ad46e10b07f077b5dbe798f178656d2e
Tyler Watson <tylercwatson@gmail.com>
2019-10-16 18:55:07 -0500

    add chest and acorn transferring

---
+++ Acorn Arena/Assets/Scripts/ChestController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChestController : MonoBehaviour
+{
+    public int numberOfAcorns = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag.Equals("Player"))
+        {
+            var player = collision.gameObject.GetComponent<PlayerController>();
+            numberOfAcorns += player.numberOfAcorns;
+            player.numberOfAcorns = 0;
+        }
+    }
+}
---
+++ Acorn Arena/Assets/Scripts/ChestScoreDisplay.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChestScoreDisplay : MonoBehaviour
+{
+    public Text scoreLabel;
+    void Update()
+    {
+        Vector2 scorePosition = Camera.main.WorldToScreenPoint(this.transform.position);
+        scoreLabel.transform.position = scorePosition;
+        scoreLabel.text = gameObject.GetComponentInParent<ChestController>().numberOfAcorns.ToString();
+    }
+}
