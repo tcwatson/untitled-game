afbb50cac8ef0ddeab68f6079b952e6e84e1b59c
Tyler Watson <tylercwatson@gmail.com>
2019-10-15 23:56:54 -0500

    add spawn locations and random spawning for acorns

---
+++ Acorn Arena/Assets/Scripts/AcornSpawner.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AcornSpawner : MonoBehaviour
+{
+    public Transform[] spawnPoints;
+    public float spawnTime = 3f;
+    public GameObject acorn;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("Spawn", spawnTime, spawnTime);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void Spawn()
+    {
+        int spawnPointIndex = Random.Range(0, spawnPoints.Length);
+        Instantiate(acorn, spawnPoints[spawnPointIndex].position, spawnPoints[spawnPointIndex].rotation);
+    }
+}
